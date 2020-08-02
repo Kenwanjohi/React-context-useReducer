076a2b2850b02e71ca5e89909f59ed5401c50c34
Kenwanjohi <wanjohiken85@gmail.com>
2020-08-02 14:44:55 +0300

    initial commit- useReducer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,149 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useReducer, useState} from 'react';
+import uuid from 'uuid/v4';
+ 
+const initialTodos = [
+  {
+    id: uuid(),
+    task: 'Learn React',
+    complete: true,
+  },
+  {
+    id: uuid(),
+    task: 'Learn Firebase',
+    complete: true,
+  },
+  {
+    id: uuid(),
+    task: 'Learn GraphQL',
+    complete: true,
+  },
+];
+const todoReducer = (state, action) => {
+  switch (action.type) {
+    case 'DO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: true };
+        } else {
+          return todo;
+        }
+      });
+    case 'UNDO_TODO':
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          return { ...todo, complete: false };
+        } else {
+          return todo;
+        }
+      });
+    case 'ADD_TODO':
+      return state.concat({
+        task: action.task,
+        id: action.id,
+        complete: false,
+      });
+    default:
+      throw new Error();
+  }
+};
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case 'SHOW_ALL':
+      return 'ALL';
+    case 'SHOW_COMPLETE':
+      return 'COMPLETE';
+    case 'SHOW_INCOMPLETE':
+      return 'INCOMPLETE';
+    default:
+      throw new Error();
+  }
+};
 function App() {
+  const [todos, dispatchTodos] = useReducer(todoReducer, initialTodos);
+  const [filter, dispatchFilter] = useReducer(filterReducer, 'ALL');
+  const [task, setTask] = useState('');
+  const handleChangeInput = event => {
+    setTask(event.target.value);
+  };
+  const handleChangeCheckbox = todo => {
+    dispatchTodos({
+      type: todo.complete ? 'UNDO_TODO' : 'DO_TODO',
+      id: todo.id,
+    });
+  };
+  const handleShowAll = () => {
+    dispatchFilter({ type: 'SHOW_ALL' });
+  };
+ 
+  const handleShowComplete = () => {
+    dispatchFilter({ type: 'SHOW_COMPLETE' });
+  };
+ 
+  const handleShowIncomplete = () => {
+    dispatchFilter({ type: 'SHOW_INCOMPLETE' });
+  };
+ 
+  const filteredTodos = todos.filter(todo => {
+    if (filter === 'ALL') {
+      return true;
+    }
+ 
+    if (filter === 'COMPLETE' && todo.complete) {
+      return true;
+    }
+ 
+    if (filter === 'INCOMPLETE' && !todo.complete) {
+      return true;
+    }
+ 
+    return false;
+  });
+
+  const handleSubmit = event => {
+    if (task) {
+      dispatchTodos({ type: 'ADD_TODO', task, id: uuid() });
+    }
+ 
+    setTask('');
+ 
+    event.preventDefault();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button type="button" onClick={handleShowAll}>
+          Show All
+        </button>
+        <button type="button" onClick={handleShowComplete}>
+          Show Complete
+        </button>
+        <button type="button" onClick={handleShowIncomplete}>
+          Show Incomplete
+        </button>
+      </div>
+      <ul>
+        {filteredTodos.map(todo => (
+          <li key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.complete}
+                onChange={() => handleChangeCheckbox(todo)}
+              />
+              {todo.task}
+            </label>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={task}
+          onChange={handleChangeInput}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
     </div>
   );
 }
